3e596c8dec36f37886e7583c4523721f43e97f14
Kayzen24 <106814715+Kayzen24@users.noreply.github.com>
2022-06-05 12:11:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
-global.packname = 'Bot Whatsapp'
-global.author = 'TRASH'
+global.owner = ['6281213184348','0']
+global.packname = 'KayX Bot WhatsApp'
+global.author = 'KayX | Kayzen'
 global.sessionName = 'data'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'KayX'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
